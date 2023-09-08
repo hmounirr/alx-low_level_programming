a2495d9cf9dc1c15d21041fdd0b739dc9695f184
hmounirr <mounir02houssam@gmail.com>
2023-09-08 09:28:16 -0700

    Alphabet printing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char cara = 'a';
+	int i = 0;
+
+	/* my code goes here */
+
+	while (i < 26)
+	{
+		putchar(cara);
+		cara++;
+		i++;
+	}
+	return (0);
+}
