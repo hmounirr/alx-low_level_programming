0582e0749274bd9963ca3ee394cb7bbd3176fc64
hmounirr <mounir02houssam@gmail.com>
2023-09-08 10:53:10 -0700

    Conversion from integer to char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	i = char(i);
+
+	/* my code goes here */
+	while (i < '10')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
