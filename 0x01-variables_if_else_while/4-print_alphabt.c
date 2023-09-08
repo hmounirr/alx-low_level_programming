d6fb231eed2e938ca28a594d093be615f57840f1
hmounirr <mounir02houssam@gmail.com>
2023-09-08 10:00:44 -0700

    Get rid of that e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print alphabet in lowercase
+ *        except 'e' and 'q'.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char cara = 'a';
+	int i = 0;
+
+	/* my code goes here */
+
+	while (i < 26)
+	{
+		if (cara == 'e' || cara == 'q')
+			cara++;
+		else
+		{
+			putchar(cara);
+			cara++;
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
