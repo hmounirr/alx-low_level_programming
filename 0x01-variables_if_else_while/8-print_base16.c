21e0d52626b53931df55ed473da50c1651b61309
hmounirr <mounir02houssam@gmail.com>
2023-09-08 11:04:40 -0700

    Numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print in lowercase
+ *         all the numbers of base 16.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char cara = 'a';
+	int i = 0;
+
+	/* my code goes here */
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 0;
+	while (i < 6)
+	{
+		putchar(cara);
+		cara++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
