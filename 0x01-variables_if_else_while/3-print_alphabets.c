f5ab16401cac4e7136e550fc43565bff8c341f74
hmounirr <mounir02houssam@gmail.com>
2023-09-08 09:44:55 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print alphabet in lowercase
+ *        then in uppercase.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char cara = 'a';
+	int i = 0;
+
+	/* my code goes here */
+
+	while (i < 26)
+	{
+		putchar(cara);
+		cara++;
+		i++;
+	}
+	cara = 'a';
+	i = 0;
+	cara = cara - 34;
+	while (i <= 26)
+	{
+		putchar(cara);
+		cara++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
