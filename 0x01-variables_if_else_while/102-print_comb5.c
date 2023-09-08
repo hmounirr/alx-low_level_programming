14835889779191320964177b28ef3ae3a6ceeeaf
hmounirr <mounir02houssam@gmail.com>
2023-09-08 13:46:50 -0700

    4Digitz

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes here */
+/**
+ * main - Print combinations of
+ *        two two digits of numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	/* my code goes here */
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (j < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
