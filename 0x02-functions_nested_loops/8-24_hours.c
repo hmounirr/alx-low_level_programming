b4e929f3edec074eadc0db721de0f176fb05f46e
hmounirr <mounir02houssam@gmail.com>
2023-09-12 07:48:58 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - a Function that prints Jack Bauer's every minute
+ *             of his day
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+		for (j = 0; j <= 59; j++)
+		{
+			if (i >= 0 && i < 10)
+			{
+				_putchar(0 + '0');
+				_putchar(i + '0');
+			}
+			else
+				_putchar(i + '0');
+			_putchar(':');
+			if (j >=0 && j < 10)
+			{
+				_putchar(0 + '0');
+				_putchar(j + '0');
+			}
+			else
+				_putchar(j + '0');
+		}
+	return (0);
+}
