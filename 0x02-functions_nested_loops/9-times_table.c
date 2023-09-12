7ca389c27f5224ba69f220085444b55a59ed58db
hmounirr <mounir02houssam@gmail.com>
2023-09-12 08:46:20 -0700

    Nein x Nein

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,8 +17,13 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) / 10 + '0');
-			_putchar((i * j) % 10 + '0');
+			if ((i * j) < 10)
+				_putchar((i * j) + '0');
+			else
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
 			if (j < 9)
 			{
 				_putchar(',');
