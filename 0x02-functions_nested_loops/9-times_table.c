de4027b36ba464cc6f1e0674c6c67d872c2b31eb
hmounirr <mounir02houssam@gmail.com>
2023-09-12 08:56:02 -0700

    Nein x Nein

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,22 +18,24 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			if ((i * j) < 10)
+			{
 				_putchar((i * j) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
 			else
 			{
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
