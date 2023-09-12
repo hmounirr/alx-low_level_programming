da254f1d8cae34891bba01901f1712720f55e36c
hmounirr <mounir02houssam@gmail.com>
2023-09-12 14:52:07 -0700

    My commit for Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - Prints 50 numbers of the Fibonacci suite
- *
- * Return: Always 0.
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long int Un;
-	int i;
-	long int U1 = 1;
-	long int U2 = 2;
+	long int i, j, k, next;
 
-	/* my code goes here */
-	printf("%ld, %ld, ", U1, U2);
-	for (i = 1; i <= 50; i++)
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
 	{
-
-		Un = U1 + U2;
-		U1 = U2;
-		U2 = Un;
-		printf(", %ld", Un);
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-	printf("\n");
+
 	return (0);
 }
