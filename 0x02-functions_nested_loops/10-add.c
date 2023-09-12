b3b6fb5c8cc2cb454b35c23178af519c94fafbb4
hmounirr <mounir02houssam@gmail.com>
2023-09-12 10:52:36 -0700

    AddTwoNumberz

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,17 +2,13 @@
 
 /**
  * add - adds two integers and output the result
- * @i,@j: two integers that the user insert
+ * @a: two integers that the user insert
+ * @b: second integer
  *
  * Return: Returns the sum of the two intgers
  */
 
 int add(int a, int b)
 {
-	int a;
-	int b;
-	int sum;
-
-	sum = a + b;
-	return (sum);
+	return (a + b);
 }
