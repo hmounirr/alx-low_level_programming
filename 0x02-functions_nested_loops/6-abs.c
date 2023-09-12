a06e7b509568a08fe20c3e8b866cb9528557f1c5
hmounirr <mounir02houssam@gmail.com>
2023-09-12 06:40:35 -0700

    FromNegativetoPositive

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,13 +10,12 @@
 
 int _abs(int r)
 {
-	if (r > 0)
-		return (r);
-	else if (r == 0)
-		return (r);
-	else
+	if (r < 0)
 	{
 		r = abs(r);
 		return (r);
 	}
+	else if (r >= 0)
+		return (r);
+	return (0);
 }
