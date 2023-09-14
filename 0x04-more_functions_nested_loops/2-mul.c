8e694579888bda39f45f97c3f20388f1f49e6983
hmounirr <mounir02houssam@gmail.com>
2023-09-14 22:25:25 +0100

    Do you love multiplication?

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns the result of multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
