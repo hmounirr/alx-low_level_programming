114a870be4aca698df5e45cf6c0d489fc6c0d32c
hmounirr <mounir02houssam@gmail.com>
2023-09-14 23:10:43 +0100

    More Numberz

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,15 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
-			if (j == 14)
-				_putchar('\n');
+			if (j / 10 == 1)
+				_putchar(j / 10 + '0');
+			else if (j % 10)
+				_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
