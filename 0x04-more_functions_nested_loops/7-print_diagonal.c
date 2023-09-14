e49db408ef9acfa4a2bfae4425805d087bc489a2
hmounirr <mounir02houssam@gmail.com>
2023-09-14 23:51:39 +0100

    Line Digonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,17 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			_putchar('\\');
-			_putchar('\n');
-			_putchar(i * ' ' + '0');
+			_putchar(' ');
 		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
