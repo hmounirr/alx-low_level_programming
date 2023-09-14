485905061f6564031d4835afdfe78d3521b5852e
hmounirr <mounir02houssam@gmail.com>
2023-09-14 22:21:42 +0100

    Is it a digit or not?

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit - checks if the variable inserted is a digit or not
- * @c: variable that the user insert
+ * @c: variable that the user insert.
  *
  * Return: 1 if c is a digit otherwise it returns 0.
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
