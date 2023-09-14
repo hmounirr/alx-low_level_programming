1b2a8179f39b8c72d7ca5ea2611a8af67048a021
hmounirr <mounir02houssam@gmail.com>
2023-09-15 00:20:01 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-			continue;
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-			continue;
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-			continue;
-		}
 		else
 			printf("%d", i);
-		printf(" ");
+		if (i < 100)
+			printf(" ");
 	}
 	return (0);
 }
