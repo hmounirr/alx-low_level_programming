77ca900bafefb3fef276d9f17c9431cef27d87e9
hmounirr <mounir02houssam@gmail.com>
2023-09-19 19:18:03 +0100

    Reverse___esreveR

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,17 +10,18 @@
 
 void print_rev(char *s)
 {
-	int i, j;
-	char temp;
+	int i;
+	int length = strlen(s);
 
-	for (i = strlen(s) - 1; i >= 0; i--)
+	/*for (i = strlen(s) - 1; i >= 0; i--)
 		for (j = 0; j < i; j++)
 		{
 			temp = s[j + 1];
 			s[j + 1] = s[j];
 			s[j] = temp;
 		}
-	for (i = 0; s[i] != '\0'; i++)
+		*/
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
