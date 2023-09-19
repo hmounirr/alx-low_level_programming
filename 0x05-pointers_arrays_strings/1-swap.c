221357aea91850e8a793587bdbfdde1497790fb9
hmounirr <mounir02houssam@gmail.com>
2023-09-19 11:31:08 +0100

    Swap like God

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the value of two integers
+ * @a: pointer that points on the first integer
+ * @b: pointer that points on the second integer
+ *
+ * Return: Nor return (void)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+        temp = *b;
+	*b = *a;
+	*a = temp;
+}
