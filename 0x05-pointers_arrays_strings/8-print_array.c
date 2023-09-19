0292d1e5020fd2c0817a355797eed2ea1e0773b0
hmounirr <mounir02houssam@gmail.com>
2023-09-19 20:41:58 +0100

    Dont worry :)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,9 +17,9 @@ void print_array(int *a, int n)
 		printf("%d", a[i]);
 		if (i < n - 1)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf(",");
+			printf(" ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
