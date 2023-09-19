2ba6ec7b38149e7f8ff1d76f15c5a39b91a8cff7
hmounirr <mounir02houssam@gmail.com>
2023-09-19 20:59:39 +0100

    My copy is done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcpy - Function that copies the content of string whos pointer is src
- * @dest: pointer that points at our buffer 
+ * @dest: pointer that points at our buffer
  * @src: pointer that points at our source string thats gonna be coppied
  *
  * Return: the adress of pointer dest to view the content of the string
