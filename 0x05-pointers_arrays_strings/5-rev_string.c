a26223af8e990e6f5c83a95884f9e93b6913f4aa
hmounirr <mounir02houssam@gmail.com>
2023-09-19 19:30:05 +0100

    Reverse_data

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * print_rev - Function that prints a string in reverse
+ * rev_string - Function that prints a string in reverse
  * @s: pointer that points on the first character of our string
  *
  * Return: No return only prints
@@ -10,15 +10,15 @@
 
 void rev_string(char *s)
 {
-        int i, j;
-        char temp;
+	int i, j;
+	char temp;
 
-        for (i = strlen(s) - 1; i >= 0; i--)
-                for (j = 0; j < i; j++)
-                {
-                        temp = s[j + 1];
-                        s[j + 1] = s[j];
-                        s[j] = temp;
-                }
+	for (i = strlen(s) - 1; i >= 0; i--)
+		for (j = 0; j < i; j++)
+		{
+			temp = s[j + 1];
+			s[j + 1] = s[j];
+			s[j] = temp;
+		}
 }
 
