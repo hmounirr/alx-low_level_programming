1d56a9046a76db84dfb20b432fe05d87de28c46c
hmounirr <mounir02houssam@gmail.com>
2023-09-19 13:18:21 +0100

    REVERE____ESREVER

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Function that prints a string in reverse
+ * @s: pointer that points on the first character of our string
+ *
+ * Return: No return only prints
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int len;
+	char temp;
+
+	len = _strlen(s);
+	while (s[i] != '\0')
+	{
+		temp = s[len];
+		s[len] = s[i];
+		s[i] = temp;
+		i++;
+		len--;
+	}
+}
