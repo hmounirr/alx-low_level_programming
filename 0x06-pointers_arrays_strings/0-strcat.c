5e9dd2651b9b11f968d82259a38fd478b87d0eb7
hmounirr <mounir02houssam@gmail.com>
2023-09-21 18:13:23 +0100

    Rebuilding strcat from scratch

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,17 +15,16 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
-	
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
+
 	dest[i] = '\0';
-	
+
 	return (dest);
 }
