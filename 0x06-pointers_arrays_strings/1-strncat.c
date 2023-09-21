6651b57bb1b3e5c38052dd04c180c0dc5249c59a
hmounirr <mounir02houssam@gmail.com>
2023-09-21 18:30:35 +0100

    strnncat with n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,12 +19,13 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 		i++;
-	while (j <= n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
 
+	dest[i] = '\0';
 	return (dest);
 }
