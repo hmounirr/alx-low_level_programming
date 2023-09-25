5ac9bf63efc46455ece774a9acdfcda8e571b346
hmounirr <mounir02houssam@gmail.com>
2023-09-25 21:40:14 +0100

    HEY copy this using memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copies memory area
+ * @dest: destinated string
+ * @src: source string
+ * @n: n bytes thats gonna be copied
+ *
+ * Return: address in memory of string dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest [i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
