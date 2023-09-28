e7e705b4715d99031200264241fe9fcc08607825
hmounirr <mounir02houssam@gmail.com>
2023-09-28 17:00:39 +0100

    Rebuilding atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,19 +12,17 @@ int _atoi(char *s)
 	int i = 0;
 	unsigned int digit = 0, sign = 1;
 
-	while (s[i] < '0' || s[i] > '9')
+	while (s[i] > '0' || s[i] < '9')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
+	} while (s[i] >= '0' && s[i] <= '9')
 	{
 		digit = digit * 10 + (s[i] - '0');
 		i++;
 	}
-	if (digit == 0)
-		return (0);
+	digit *= sign;
 
-	return (digit * sign);
+	return (digit);
 }
