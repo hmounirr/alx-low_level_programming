ccf6d36a7d9a3117e986035d9e645969a5545480
hmounirr <mounir02houssam@gmail.com>
2023-10-01 18:26:31 +0100

    Task 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,18 +9,17 @@
  */
 int heron_sqrt(int n, double close)
 {
-    double next_num = 0.5 * (close + n / close);
+	double next_num = 0.5 * (close + n / close);
 
-    if (fabs(next_num - close) < 1e-5)
-    {
-        int new_num = (int)next_num;
-        if (new_num * new_num == n)
-            return new_num;
-        else
-            return (-1);
-    }
-
-    return heron_sqrt(n, next_num);
+	if (fabs(next_num - close) < 1e-5)
+	{
+		int new_num = (int)next_num;
+		if (new_num * new_num == n)
+			return new_num;
+		else
+			return (-1);
+	}
+	return heron_sqrt(n, next_num);
 }
 
 /**
